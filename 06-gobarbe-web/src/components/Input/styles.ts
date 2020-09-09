11587b0fdc15e429ce011e41c43f57018ae07348
caiolucasb <caio.lucas75@gmail.com>
2020-09-08 23:17:16 -0300

    creating button and input components

---
+++ 06-gobarbe-web/src/components/Input/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    background: #232129;
+    border-radius: 10px;
+    border: 2px solid #232129;
+    padding: 16px;
+    width: 100%;
+    color: #666360;
+
+    display: flex;
+    align-items: center;
+
+    & + div {
+        margin-top: 8px;            
+    } 
+
+input{
+    background: transparent;
+    flex: 1;
+    border: 0;
+    color: #f4ede8;
+
+    &::placeholder {
+        color: #666360;
+    }
+}
+
+    svg {
+        margin-right: 16px;
+    }
+`;
