0783980b3e117bc6b2f6d100f8a274acb6afb049
caiolucasb <caio.lucas75@gmail.com>
2020-09-10 22:09:04 -0300

    creating error styled component

--- 06-gobarbe-web/src/components/Input/index.tsx
+++ 06-gobarbe-web/src/components/Input/index.tsx
@@ -1,8 +1,9 @@
 import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
 
-import { Container } from './styles'
+import { Container, Error } from './styles'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
@@ -35,13 +36,20 @@ const Input: React.FC<InputProps>= ({name, icon: Icon, ...rest}) => {
     }, [fieldName, registerField])
 
     return (
-    <Container isFilled={isFilled} isFocused={isFocused}>
+    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
         <Icon size={20}/>
         <input 
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
         defaultValue={defaultValue} 
         ref={inputRef} {...rest} />
+        {error && 
+            (
+            <Error title={error}>
+                <FiAlertCircle color="#c53030" size={20} />
+            </Error>
+            )
+            }
     </Container>
     );
 }
--- 06-gobarbe-web/src/components/Input/styles.ts
+++ 06-gobarbe-web/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components'
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
      isFocused: boolean;
      isFilled: boolean;
+     isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -19,6 +22,10 @@ export const Container = styled.div<ContainerProps>`
     & + div {
         margin-top: 8px;            
     } 
+
+    ${props => props.isErrored && css`
+        border-color: #c53030;
+    `}
 
     ${props => props.isFocused && css`
         color: #ff9000;
@@ -43,4 +50,23 @@ input{
     svg {
         margin-right: 16px;
     }
+`;
+
+export const Error = styled(Tooltip)`
+    height: 20px;
+    margin-left: 16px;
+    svg {
+        margin: 0;
+    }
+
+    span{
+        background: #c53030;
+        color: #FFF;
+
+        &::before {
+            border-color: #c53030 transparent;
+        }
+
+    }
+
 `;
