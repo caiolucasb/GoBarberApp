7e25e052bebc15275d3d9e08873ef416f49f1806
caiolucasb <caio.lucas75@gmail.com>
2020-09-13 11:08:04 -0300

    using context api

--- 06-gobarbe-web/src/App.tsx
+++ 06-gobarbe-web/src/App.tsx
@@ -4,10 +4,16 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
+import {AuthProvider} from './hooks/AuthContext'
+
+
+
 const App: React.FC = () => (
   <>
+  <AuthProvider>
   <SignIn />
   <GlobalStyle/>
+  </AuthProvider>
   </>
 )
 export default App;
--- 06-gobarbe-web/src/pages/SignIn/index.tsx
+++ 06-gobarbe-web/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 import * as Yup from 'yup';
@@ -11,11 +11,22 @@ import Input from '../../components/Input';
 import { Container, Content, Background } from './styles';
 
 import getValidationErrors from '../../utils/getValidationErrors';
+import {useAuth} from '../../hooks/AuthContext';
+
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
+
+
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
-    const handleSubmit = useCallback(async (data: object) => {
+    const { user, signIn } = useAuth();
+
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
         try{
             formRef.current?.setErrors({});
             const schema = Yup.object().shape({
@@ -26,11 +37,15 @@ const SignIn: React.FC = () => {
             await schema.validate(data,{
                 abortEarly: false,
             });
+            signIn({
+                email: data.email,
+                password: data.password,
+            });
         }catch(err){
             const errors = getValidationErrors(err);
             formRef.current?.setErrors(errors)
         }
-    }, [])
+    }, [signIn])
 
     return (
         <Container>
